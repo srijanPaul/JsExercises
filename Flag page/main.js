fe9810d4de9158732cf6af173559310cb926d160
TiltproofRain <50487716+srijanPaul@users.noreply.github.com>
2019-06-02 00:44:09 +0530

    Add files via upload

--- Flag page/main.js
+++ Flag page/main.js
@@ -1,6 +1,5 @@
 var names = ["Great Britian" , "Japan", "India", "Namibia", "South Sudan", "Canada"];
-let flags = [];
-
+var colors = ["#F9A6A6", "#E1BADF", "#c7a7f0", "#68F476", "#F49768", "#F9E252", "#9BEDB9", "#9BEDE5", "#A6C7F9", "#C2A6F9", "#F9A6D5"];
 
 function showname(num){
     document.getElementById("textbox").innerHTML = names[num];
@@ -9,3 +8,9 @@ function showname(num){
 function clearText(){
     document.getElementById("textbox").innerHTML = "";
 }
+
+function changePageColor(){
+    var color = colors[Math.floor(Math.random()*colors.length)];
+    document.querySelectorAll("*").forEach(e => e.style = "background-color : " + color);
+}
+
